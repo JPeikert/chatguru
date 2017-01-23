d7ccccd5644c91f741f45b43f31ed6ae39e8dafe
Maciej Gąska <maciej.gaska@netguru.pl>
2017-01-23 13:23:37 +0100

    update initializers

--- config/initializers/sprockets_commoner.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.assets.configure do |env|
-  Sprockets::Commoner::Processor.configure(
-    env,
-    include: ['app/assets/javascript'],
-    exclude: ['vendor/bundle'],
-    babel_exclude: [/node_modules/]
-  )
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations' }
   root 'rooms#index'
-
   get 'react_examples/component', to: 'react_examples#component', as: :component
   resources :rooms, only: %w(index show)
-  resources :messages, only: :create
+  resources :messages, only: %w(index create)
+
+  get '/chat', to: 'chatroom#show'
+
+  mount ActionCable.server => '/cable'
 end
